49e8ba58a29b5aa9e2b650011f38a548c857158c
anisova <39698859+anisova@users.noreply.github.com>
2021-03-23 12:32:45 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+//функция определения поддержки WebP
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  } else {
+    document.querySelector("body").classList.add("no-webp");
+  }
+});
+// Функция работы модального окна
+function toggleModal(modalWindow, openButton, closeButton) {
+  const openBtn = document.querySelectorAll(openButton);
+  const modal = document.querySelector(modalWindow);  
+  openBtn.forEach(element => {       
+    const closeBtn = modal.querySelector(closeButton);    
+    element.addEventListener("click", (e) => {
+      console.log(element);      
+      let paddingoffset=window.innerWidth- document.body.offsetWidth + 'px'; 
+      e.preventDefault();
+      modal.style.display = "flex";
+      document.body.style.overflow = "hidden";
+      document.body.style.paddingRight = paddingoffset;      
+    });
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+      document.body.style.overflow = "";
+      document.body.style.paddingRight = "";
+    });    
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        closeBtn.click();
+      }
+    });    
+  });    
+}
+toggleModal(".modal", ".btn", ".btn-close");
+
+// function formAction () { 
+// let form = document.querySelector('.form');
+
+// form.addEventListener('submit', function(event) {
+// 	let promise = fetch('/ajax/', {
+// 		method: 'POST',
+// 		body: new FormData(this),
+// 	});
+	
+// 	promise.then(
+// 		response => {
+// 			return response.text();
+// 		}
+// 	).then(
+// 		text => {
+// 			alert(text);
+// 		}
+// 	);	
+// 	event.preventDefault();
+// });}
+// formAction();
+
